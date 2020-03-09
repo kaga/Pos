f37bcffb0fd97ee6a9cb626a6e443a2ce16cb82d
kaga <kaga21@gmail.com>
2020-03-10 10:34:11 +1300

    add missing test file

---
+++ VoyagerPosTest/PricingCondition/BaseUnitPricingConditionTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NUnit.Framework;
+using VoyagerPos;
+
+namespace VoyagerPosTest.PricingCondition
+{
+    public class BaseUnitPricingConditionTest
+    {
+        BaseUnitPricingCondition condition;
+
+        [SetUp]
+        public void Setup()
+        {
+            condition = new BaseUnitPricingCondition(unitPrice: 1.25M);
+        }
+
+        [Test]
+        public void testPriceCalculation()
+        {
+            var result = condition.CalculatePrice(1);
+            Assert.AreEqual(1.25M, result.subtotal);
+            Assert.AreEqual(0, result.remainingQuantity, message: "remainingQuantity should always return 0");
+
+            result = condition.CalculatePrice(2);
+            Assert.AreEqual(2.5M, result.subtotal);
+            Assert.AreEqual(0, result.remainingQuantity, message: "remainingQuantity should always return 0");
+        }
+
+        [Test]
+        public void testInvalidQuantity()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => condition.CalculatePrice(quantity: 0));
+            Assert.Throws<ArgumentOutOfRangeException>(() => condition.CalculatePrice(quantity: -1));          
+        }
+    }
+}
